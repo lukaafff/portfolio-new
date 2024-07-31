aa71961e363fc9a2a488f67add871d5fbad23f7b
lukaafff <luizagarcia2405@gmail.com>
2024-07-31 20:21:37 -0300

    funcionamento do email

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import styles from './Contato.module.css';
 
 function Contato() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1duus7m', 'template_btj51d5', form.current, 'wWVSvqU--9Mtucd56')
+      .then((result) => {
+          console.log(result.text);
+          alert('Mensagem enviada com sucesso!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Ocorreu um erro ao enviar a mensagem. Tente novamente.');
+      });
+  };
+
   return (
     <section id="contato" className={styles.container}>
       <div className="titleWrapper">
@@ -10,23 +26,22 @@ function Contato() {
       </div>
 
       <p className={styles.subtitle}>Vamos conversar!</p>
-      <form className={styles.form}>
-
+      <form ref={form} onSubmit={sendEmail} className={styles.form}>
         <div className={styles.inputGroupRow}>
           <div className={styles.inputGroup}>
             <label htmlFor="name">Nome:</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="user_name" required />
           </div>
 
           <div className={styles.inputGroup}>
             <label htmlFor="email">E-mail:</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="user_email" required />
           </div>
         </div>
 
         <div className={`${styles.inputGroup} ${styles.fullWidth}`}>
           <label htmlFor="message">Mensagem:</label>
-          <textarea id="message" name="message"></textarea>
+          <textarea id="message" name="message" required></textarea>
         </div>
 
         <button type="submit" className={styles.submitButton}>Enviar</button>
