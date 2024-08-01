37701b9c2b0d86c188910abca5cc9f17441b4cbf
lukaafff <luizagarcia2405@gmail.com>
2024-07-31 21:08:50 -0300

    adicionando curriculo

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
 import styleInicio from './Inicio.module.css';
-import avatar from '../../assets/imgs/luiza.jpg'
+import avatar from '../../assets/imgs/luiza.jpg';
+import curriculo from '../../../public/Luiza_Nascimento Garcia_Curriculo_2024.pdf';
 
 const Inicio = () => {
   return (
     <section id="inicio" className={styleInicio.container}>
-
-        <div className={styleInicio.content}>
-            <h1>Olá, eu sou a</h1>
-            <h2>Luiza</h2>
-            <p>Designer & Desenvolvedora web fullstack</p>
-            <button className={styleInicio.iconBox}>Currículo <i className='bx bx-chevron-down'></i></button>
-        </div>
-
-        <img className={styleInicio.avatar} src={avatar} alt="Avatar Luiza Garcia" />
+      <div className={styleInicio.content}>
+        <h1>Olá, eu sou a</h1>
+        <h2>Luiza</h2>
+        <p>Designer & Desenvolvedora web fullstack</p>
+        <a href={curriculo} download className={styleInicio.iconBox}>
+          Currículo <i className='bx bx-chevron-down'></i>
+        </a>
+      </div>
+      <img className={styleInicio.avatar} src={avatar} alt="Avatar Luiza Garcia" />
     </section>
-  )
+  );
 }
 
-export default Inicio
+export default Inicio;
